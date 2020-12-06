1c78cf7ef56d8663779fe42f51afee123e28e9a9
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2020-12-06 22:35:50 +0200

    Update Android project

---
+++ build/static/js/53.eb069a5c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgeonode-mobile-sample"]=this["webpackJsonpgeonode-mobile-sample"]||[]).push([[53],{169:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(15),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.eb069a5c.chunk.js.map
